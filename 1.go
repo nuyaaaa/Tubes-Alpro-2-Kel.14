963ea9fcad9fe091deedfd09d0d712f8826a3993
nuyaaaa <nabilahanif0512@gmail.com>
2026-05-13 21:38:46 +0700

    template buatPS C:\Users\HYPE AMD\Downloads\ngoding> git add 1.go tubes alpro 2

--- 1.go
+++ 1.go
@@ -2,47 +2,16 @@ package main
 
 import "fmt"
 
-const NMAX = 1001
-type identitas struct {
-	nama string
-	tahun int
-	asal string
+const NMAX = a
+type x struct {
+	
 }
-type tabPenduduk [NMAX]identitas
+type y [NMAX]x
 
 func main() {
-	var n, targetTahun int
-	var arr tabPenduduk
-	fmt.Scan(&n)
-	for i := 0; i < n; i++ {
-		fmt.Scan(&arr[i].nama, &arr[i].tahun, &arr[i].asal)
-	}
-	fmt.Scan(&targetTahun)
-	cariTarget(arr, n, targetTahun)
+	var
 }
 
-func cariTarget(arr tabPenduduk, n, X int) {
-	var awal, akhir, tengah int
-	var found bool
-	awal = 0
-	akhir = n - 1
-	tengah = (awal + akhir) / 2
-	for awal <= akhir && !found {
-		if X == arr[tengah].tahun {
-			found = true
-		} else if X < arr[tengah].tahun {
-			akhir = tengah - 1
-		} else {
-			awal = tengah + 1
-		}
-		tengah = (awal + akhir) / 2
-	}
-	if found {
-		fmt.Print(arr[tengah].nama)
-		fmt.Printf("\n%d", X)
-		fmt.Printf("\n%s", arr[tengah].asal)
-		fmt.Printf("\ndetemukan di index ke-%d", tengah)
-	} else {
-		fmt.Print("Data tidak ditemukan")
-	}
+func b(c) {
+	
 }
