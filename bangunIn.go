1527a3184a5d5bbc99d50e7d68094cc49adc7301
nuyaaaa <nabilahanif0512@gmail.com>
2026-05-24 18:31:19 +0700

    nyicil

--- bangunIn.go
+++ bangunIn.go
@@ -4,15 +4,121 @@ import "fmt"
 
 const NMAX int = 100
 
-type x struct {
+type kontak struct {
+	Telepon, Email, Lokasi string
 }
-type y [NMAX]x
+
+type layanan struct {
+	JenisMaterial string
+	Rating        float64
+	RiwayatOrder  int
+}
+
+type supplier struct {
+	ID            int
+	NamaPT        string
+	DetailKontak  kontak
+	DetailLayanan layanan
+}
+
+type DatabaseSUpplier [NMAX]supplier
 
 func main() {
-	menuUtama()
+	var db DatabaseSUpplier
+	var jumlahData int = 0
+
+	//paggil data dummy
+	isiDataDummy(&db, &jumlahData)
+
+	for {
+		menu := menuUtama(&db, &jumlahData)
+		if !menu {
+			break
+		}
+	}
+
 }
 
-func menuUtama() {
+func isiDataDummy(db *DatabaseSUpplier, jumlahData *int) {
+	db[*jumlahData] = supplier{
+		//cara ngisinya ikut template ini {1, "PT_Cahaya", kontak{"021-098-010", "email@.com", "Jakarta"},layanan{ "Semen", 4.5, 16}}
+	}
+	*jumlahData++
+
+	db[*jumlahData] = supplier{}
+	*jumlahData++
+
+	db[*jumlahData] = supplier{}
+	*jumlahData++
+
+	db[*jumlahData] = supplier{}
+	*jumlahData++
+
+	db[*jumlahData] = supplier{}
+	*jumlahData++
+
+	db[*jumlahData] = supplier{}
+	*jumlahData++
+
+	db[*jumlahData] = supplier{}
+	*jumlahData++
+
+	db[*jumlahData] = supplier{}
+	*jumlahData++
+
+	db[*jumlahData] = supplier{}
+	*jumlahData++
+
+	db[*jumlahData] = supplier{}
+	*jumlahData++
+
+	db[*jumlahData] = supplier{}
+	*jumlahData++
+
+	db[*jumlahData] = supplier{}
+	*jumlahData++
+
+	db[*jumlahData] = supplier{}
+	*jumlahData++
+
+	db[*jumlahData] = supplier{}
+	*jumlahData++
+
+	db[*jumlahData] = supplier{}
+	*jumlahData++
+
+	db[*jumlahData] = supplier{}
+	*jumlahData++
+
+	db[*jumlahData] = supplier{}
+	*jumlahData++
+
+	db[*jumlahData] = supplier{}
+	*jumlahData++
+
+	db[*jumlahData] = supplier{}
+	*jumlahData++
+
+	db[*jumlahData] = supplier{}
+	*jumlahData++
+
+	db[*jumlahData] = supplier{}
+	*jumlahData++
+
+	db[*jumlahData] = supplier{}
+	*jumlahData++
+
+	db[*jumlahData] = supplier{}
+	*jumlahData++
+
+	db[*jumlahData] = supplier{}
+	*jumlahData++
+
+	db[*jumlahData] = supplier{}
+	*jumlahData++
+}
+
+func menuUtama(db *DatabaseSUpplier, jumlahData *int) bool {
 	var input int
 
 	fmt.Println("_____________________________________________________")
@@ -38,32 +144,68 @@ func menuUtama() {
 
 	switch input {
 	case 1:
-		tambahDataSupplier()
+		tambahDataSupplier(db, jumlahData)
 	case 2:
-		ubahDataSupplier()
+		ubahDataSupplier(db, *jumlahData)
 	case 3:
-		hapusDataSupplier()
+		hapusDataSupplier(db, jumlahData)
 	case 4:
-		tampilkanDataSupplier()
+		tampilkanDataSupplier(*db, *jumlahData)
 	case 5:
-		pencarianDataSupplier()
+		pencarianDataSupplier(*db, *jumlahData)
 	case 6:
-		pengurutanDataSupplier()
+		pengurutanDataSupplier(db, *jumlahData)
 	case 7:
-		tampilkanStatistik()
+		tampilkanStatistik(*db, *jumlahData)
 	case 8:
 		fmt.Println("Terima kasih telah menggunakan BANGUNIN.")
+		return false
 	default:
 		fmt.Println("Pilihan tidak valid. Silakan pilih menu yang tersedia.")
-		menuUtama()
 	}
+	return true
 }
 
-func tambahDataSupplier() {
+func tambahDataSupplier(db *DatabaseSUpplier, jumlahData *int) {
+	if *jumlahData >= NMAX {
+		fmt.Println("\n[GAGAL] PENUH EUY! Tidak dapat menambahkan data supplier baru.")
+		return
+	}
 
+	fmt.Println("\n=====================================================")
+	fmt.Println(" 		        TAMBAH DATA SUPPLIER                  ")
+	fmt.Println("=====================================================")
+
+	db[*jumlahData].ID = *jumlahData + 1
+	fmt.Print("ID Supplier: %d\n", db[*jumlahData].ID)
+
+	fmt.Print("Nama Perusahaan: ")
+	fmt.Scan(&db[*jumlahData].NamaPT)
+
+	fmt.Print("Nomor Telepon: ")
+	fmt.Scan(&db[*jumlahData].DetailKontak.Telepon)
+
+	fmt.Print("Email: ")
+	fmt.Scan(&db[*jumlahData].DetailKontak.Email)
+
+	fmt.Print("Lokasi: ")
+	fmt.Scan(&db[*jumlahData].DetailKontak.Lokasi)
+
+	fmt.Print("Jenis Material: ")
+	fmt.Scan(&db[*jumlahData].DetailLayanan.JenisMaterial)
+
+	fmt.Print("Rating (0.0 - 5.0): ")
+	fmt.Scan(&db[*jumlahData].DetailLayanan.Rating)
+
+	fmt.Print("Riwayat Order: ")
+	fmt.Scan(&db[*jumlahData].DetailLayanan.RiwayatOrder)
+
+	*jumlahData++
+
+	fmt.Println("\n[BERHASIL] Data supplier berhasil ditambahkan!")
 }
 
-func ubahDataSupplier() {
+func ubahDataSupplier(db *DatabaseSUpplier, jumlahData int) {
 
 }
 
