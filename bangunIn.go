f6490650619e926772af4fdd15cd3b8ebef4f711
nuyaaaa <nabilahanif0512@gmail.com>
2026-06-18 09:38:57 +0700

    benerin error handling

--- bangunIn.go
+++ bangunIn.go
@@ -52,13 +52,13 @@ func isiDataDummy(db *databaseSupplier, n *int) {
 	db[*n] = supplier{5, "PT_Bata_Merah", kontak{"024-222-123", "halo@batamerah.com", "Semarang"}, layanan{"Bata", 4.4, 110}}
 	*n++
 
-	db[*n] = supplier{6, "PT_Cat_Cerah", kontak{"061-666-123", "order@catcerah.com", "Medan"}, layanan{"Cat", 4.9, 150}}
+	db[*n] = supplier{6, "PT_Cat_Cerah", kontak{"061-666-123", "order@catcerah.com", "Bekasi"}, layanan{"Cat", 4.9, 150}}
 	*n++
 
 	db[*n] = supplier{7, "PT_Keramik_Indah", kontak{"021-555-789", "cs@keramikindah.com", "Tangerang"}, layanan{"Keramik", 4.3, 40}}
 	*n++
 
-	db[*n] = supplier{8, "PT_Besi_Kokoh", kontak{"021-888-123", "marketing@besikokoh.com", "Bekasi"}, layanan{"Besi", 4.7, 95}}
+	db[*n] = supplier{8, "PT_Besi_Kokoh", kontak{"021-888-123", "marketing@besikokoh.com", "Medan"}, layanan{"Besi", 4.7, 95}}
 	*n++
 
 	db[*n] = supplier{9, "PT_Pipa_Aliran", kontak{"021-777-123", "pipa@aliran.com", "Depok"}, layanan{"Pipa", 4.5, 75}}
@@ -92,13 +92,11 @@ func menuUtama(db *databaseSupplier, n *int) bool {
 	fmt.Println("=====================================================")
 	fmt.Print("Pilih menu (1-8): ")
 
-	_, err := fmt.Scan(&input)
+	fmt.Scan(&input)
 
-	if err != nil {
-		var sapuBuffer string
-		fmt.Scanln(&sapuBuffer)
-
-		fmt.Println("Pilihan tidak valid. Silakan masukkan angka.")
+	//buat error handling klo user masukin angka yang ga sesuai sama menu yang ada, nanti bakal keluar info buat milih menu yang tersedia
+	if input < 1 || input > 8 {
+		fmt.Println("\n[INFO] Pilihan menu tidak valid. Silakan pilih menu yang tersedia.")
 		return true
 	}
 
@@ -120,8 +118,6 @@ func menuUtama(db *databaseSupplier, n *int) bool {
 	case 8:
 		fmt.Println("Terima kasih telah menggunakan BANGUNIN.")
 		return false
-	default:
-		fmt.Println("Pilihan tidak valid. Silakan pilih menu yang tersedia.")
 	}
 	return true
 }
@@ -134,11 +130,17 @@ func tambahDataSupplier(db *databaseSupplier, n *int) {
 	}
 
 	fmt.Println("\n=====================================================")
-	fmt.Println(" 		        TAMBAH DATA SUPPLIER                  ")
+	fmt.Println("                TAMBAH DATA SUPPLIER")
 	fmt.Println("=====================================================")
 
-	db[*n].ID = *n + 1
-	fmt.Printf("ID Supplier: %d\n", db[*n].ID)
+	fmt.Print("ID Supplier: ")
+	fmt.Scan(&db[*n].ID)
+
+	//nambahin kondisi dimana klo id yang di inputkan(baru) sama/udh ada di database, maka disuruh input ulang
+	if adaID(db, *n, db[*n].ID) {
+		fmt.Println("\n[GAGAL] ID Supplier sudah digunakan. Silakan masukkan ID yang berbeda.")
+		return
+	}
 
 	fmt.Print("Nama Perusahaan: ")
 	fmt.Scan(&db[*n].namaPT)
@@ -164,6 +166,15 @@ func tambahDataSupplier(db *databaseSupplier, n *int) {
 	*n++
 
 	fmt.Println("\n[BERHASIL] Data supplier berhasil ditambahkan!")
+}
+
+func adaID(db *databaseSupplier, n int, id int) bool {
+	for i := 0; i < n; i++ {
+		if db[i].ID == id {
+			return true
+		}
+	}
+	return false
 }
 
 //mengubah data supplier yang sudah ada berdasarkan id
@@ -235,6 +246,7 @@ func hapusDataSupplier(db *databaseSupplier, n *int) {
 	fmt.Println("=====================================================")
 	fmt.Print("Masukkan ID Supplier yang ingin dihapus: ")
 	fmt.Scan(&cariID)
+
 	//ini juga pke sequential search
 	idx = -1
 	i = 0
@@ -287,18 +299,26 @@ func pencarianDataSupplier(db databaseSupplier, n int) {
 	}
 
 	fmt.Println("\n=====================================================")
-	fmt.Println(" 		         PENCARIAN DATA SUPPLIER              ")
+	fmt.Println("               PENCARIAN DATA SUPPLIER ")
 	fmt.Println("=====================================================")
 	fmt.Println("1. Cari berdasarkan Lokasi")
 	fmt.Println("2. Cari berdasarkan Jenis Material")
-	fmt.Println("Pilih metode pencarian (1-2): ")
+	fmt.Println("3. Kembali ke menu utama")
+	fmt.Println("Pilih metode pencarian (1-3): ")
 	fmt.Scan(&pilihMetode)
+
+	if pilihMetode < 1 || pilihMetode > 3 {
+		fmt.Println("\n[INFO] Pilihan metode tidak valid. Silakan pilih 1, 2, atau 3.")
+		pencarianDataSupplier(db, n)
+		return
+	}
 
 	switch pilihMetode {
 	case 1:
 		fmt.Print("Masukkan Lokasi yang ingin dicari: ")
 		fmt.Scan(&findData)
 		fmt.Println("\n-- HASIL PENCARIAN --")
+
 		//sequntial search buat nyari data yang lokasi nya sesuai sama inputan user
 		for i := 0; i < n; i++ {
 			if db[i].detailKontak.lokasi == findData {
@@ -316,48 +336,125 @@ func pencarianDataSupplier(db databaseSupplier, n int) {
 		fmt.Print("Masukkan Jenis Material yang ingin dicari: ")
 		fmt.Scan(&findData)
 		fmt.Println("\n-- HASIL PENCARIAN --")
-		//sequential search buat nyari data yang jenis material nya sesuai sama inputan user
-		for i := 0; i < n; i++ {
-			if db[i].detailLayanan.jenisMaterial == findData {
-				fmt.Printf("ID: %d | Nama PT: %s | Telepon: %s | Email: %s | Lokasi: %s | Rating: %.1f | Riwayat Order: %d\n",
-					db[i].ID, db[i].namaPT, db[i].detailKontak.telepon, db[i].detailKontak.email, db[i].detailKontak.lokasi, db[i].detailLayanan.rating, db[i].detailLayanan.riwayatOrder)
-				found = true
-			}
-		}
 
-		if !found {
+		//binarysearch buat nyari data yang jenis material nya sesuai sama inputan user
+		insertSort(&db, n)
+
+		//masuk ke binary search
+		found = false
+		left := 0
+		right := n - 1
+		mid := (left + right) / 2
+		for left <= right && !found {
+			if db[mid].detailLayanan.jenisMaterial == findData {
+				found = true
+			} else if db[mid].detailLayanan.jenisMaterial < findData {
+				left = mid + 1
+			} else {
+				right = mid - 1
+			}
+			mid = (left + right) / 2
+		}
+		if found {
+			fmt.Printf("ID: %d | Nama PT: %s | Telepon: %s | Email: %s | Lokasi: %s | Rating: %.1f | Riwayat Order: %d\n",
+				db[mid].ID, db[mid].namaPT, db[mid].detailKontak.telepon, db[mid].detailKontak.email, db[mid].detailKontak.lokasi, db[mid].detailLayanan.rating, db[mid].detailLayanan.riwayatOrder)
+		} else {
 			fmt.Println("Data supplier dengan jenis material tersebut tidak ditemukan.")
 		}
-	default:
-		fmt.Println("Pilihan metode pencarian tidak valid.")
+
+	case 3:
+		return
 	}
 }
 
-//insertion desc, ngurutin data supplier berdasarkan rating tertinggi. klo rating sama, di urutin berdasarkan riwayat order terbanyak
-func pengurutanDataSupplier(db *databaseSupplier, n int) {
+//insertion sort desc buat ngurutin data biar kepake di binary
+func insertSort(db *databaseSupplier, n int) {
 	var pass, i int
 	var temp supplier
-
 	if n == 0 {
 		fmt.Println("\n[INFO] Data supplier masih kosong. Tidak ada data yang bisa diurutkan.")
 		return
 	}
-
 	pass = 1
 	for pass <= n-1 {
 		i = pass
 		temp = db[pass]
-
-		for i > 0 && prioritas(temp, db[i-1]) {
+		for i > 0 && temp.detailLayanan.rating > db[i-1].detailLayanan.rating {
 			db[i] = db[i-1]
 			i--
 		}
 		db[i] = temp
 		pass++
 	}
+}
 
-	fmt.Println("\n[BERHASIL] Data supplier berhasil diurutkan berdasarkan rating tertinggi")
-	tampilkanDataSupplier(*db, n)
+func pengurutanDataSupplier(db *databaseSupplier, n int) {
+	var passI, i, j, passS, idx int
+	var tempI, tempS supplier
+
+	fmt.Println("\n=====================================================")
+	fmt.Println("           PENGURUTAN DATA SUPPLIER (RATING)         ")
+	fmt.Println("=====================================================")
+	fmt.Println("1. Mengurutkan data supplier berdasarkan rating tertinggi")
+	fmt.Println("2. Mengurutkan data supplier berdasarkan rating terendah")
+	fmt.Println("3. Kembali ke menu utama")
+	fmt.Print("Pilih metode pengurutan (1-3): ")
+	var pilihMetode int
+	fmt.Scan(&pilihMetode)
+
+	if n == 0 {
+		fmt.Println("\n[INFO] Data supplier masih kosong. Tidak ada data yang bisa diurutkan.")
+		return
+	}
+
+	if pilihMetode < 1 || pilihMetode > 3 {
+		fmt.Println("\n[INFO] Pilihan metode tidak valid. Silakan pilih 1, 2, atau 3.")
+		pengurutanDataSupplier(db, n)
+		return
+	}
+
+	switch pilihMetode {
+
+	case 1:
+		//insertion sort desc berdasarkan rating, klo rating sama, di urutin berdasarkan riwayat order terbanyak
+		passI = 1
+		for passI <= n-1 {
+			i = passI
+			tempI = db[passI]
+
+			for i > 0 && prioritas(tempI, db[i-1]) {
+				db[i] = db[i-1]
+				i--
+			}
+			db[i] = tempI
+			passI++
+		}
+		fmt.Println("\n[BERHASIL] Data supplier berhasil diurutkan berdasarkan rating tertinggi")
+		tampilkanDataSupplier(*db, n)
+
+	case 2:
+		//selection sort asc berdasarkan rating, klo rating sama, di urutin berdasarkan riwayat order paling sedikit
+		passS = 1
+		for passS <= n-1 {
+			idx = passS - 1
+			j = passS
+			for j < n {
+				if prioritas(db[idx], db[j]) { // ini kondisi dimana klo ratingnya sama bakal masuk ke fungsi prioritas buat ngedahuluin yang riwayat order lebih sedikit
+					idx = j
+				}
+				j++
+			}
+			tempS = db[passS-1]
+			db[passS-1] = db[idx]
+			db[idx] = tempS
+			passS++
+		}
+		fmt.Println("\n[BERHASIL] Data supplier berhasil diurutkan berdasarkan rating terendah")
+		tampilkanDataSupplier(*db, n)
+
+	case 3:
+		return
+	}
 }
 
 func prioritas(kandidat, pembanding supplier) bool {
@@ -415,3 +512,5 @@ func tampilkanStatistik(db databaseSupplier, n int) {
 		fmt.Printf("- %s : %d supplier\n", wilayahUnik[i], jumlahPerWilayah[i])
 	}
 }
+
+//klo masukin sesuatu ga sesuai sama tipe datanya, nanti bikin error handling
